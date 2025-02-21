af1a2ce549347016e0b0fe17c972aee9ae20eaea
Virus-Networks <168676321+Virus-Networks@users.noreply.github.com>
2025-02-21 15:57:03 +0530

    Update info.py

--- info.py
+++ info.py
@@ -55,7 +55,7 @@ MULTI_FSUB = [int(channel_id) for channel_id in environ.get('MULTI_FSUB', '-1002
 # ============================
 # Payment Configuration
 # ============================
-QR_CODE = environ.get('QR_CODE', 'https://telegra.ph/file/636a83147f5d5fc445647-c862e98d4a8f8d6a2b.jpg')
+QR_CODE = environ.get('QR_CODE', 'https://telegra.ph/Scan-QR-Code-to-Donate-and-Pay-Us-02-08')
 OWNER_UPI_ID = environ.get('OWNER_UPI_ID', 'gsiegsue@axl')
 
 # ============================
